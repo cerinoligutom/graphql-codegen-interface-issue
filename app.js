f9df93ad843aa3ccf448995f0aa46820671eee0e
Cerino O. Ligutom III <zeferinix@gmail.com>
2020-03-30 14:08:08 +0800

    Added ids

--- app.js
+++ app.js
@@ -18,10 +18,12 @@ const typeDefs = gql`
 
 const books = [
 	{
+		id: '1',
 		title: "Harry Potter and the Chamber of Secrets",
 		author: "J.K. Rowling"
 	},
 	{
+		id: '2',
 		title: "Jurassic Park",
 		author: "Michael Crichton"
 	}
